eb637885805236414e54bbdb4b24c7eee18f06ca
Tarik Yasar <m.tarikyasar@gmail.com>
2022-07-13 12:19:35 +0300

    Fastlane second test commit

--- app/src/main/java/com/tarikyasar/gotur/MainActivity.kt
+++ app/src/main/java/com/tarikyasar/gotur/MainActivity.kt
@@ -3,13 +3,18 @@ package com.tarikyasar.gotur
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.tarikyasar.gotur.ui.theme.GoturTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,24 +24,21 @@ class MainActivity : ComponentActivity() {
             GoturTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = Color.Black
                 ) {
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = "Götür",
+                            fontSize = 32.sp,
+                            color = Color.White,
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    GoturTheme {
-        Greeting("Android")
     }
 }
